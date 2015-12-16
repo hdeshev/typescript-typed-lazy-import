429ea5a90b8ddf2461778028f4dfc6e229e30546
Hristo Deshev <hristo@deshev.com>
2015-12-16 12:57:23 +0200

    Import single items

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 declare var require: any;
 import * as myModule from "my-module";
+import {hello} from "my-module";
 
 function main(): void {
     console.log("main");
 
     var lazyHello: typeof myModule = require("./my-module");
     lazyHello.hello();
+
+    var h: typeof hello = require("./my-module").hello;
+    h();
 }
 
 main();
